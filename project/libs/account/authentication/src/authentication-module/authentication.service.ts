5ba73b6b3375cae5b3f14decd3d19699302a7277
Mikhail <84973540+Mikhail39yanov@users.noreply.github.com>
2024-04-02 14:15:43 +0200

    update AuthenticationService, add rdo user

--- project/libs/account/authentication/src/authentication-module/authentication.service.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.service.ts
@@ -1,7 +1,58 @@
-import { Injectable } from '@nestjs/common';
-import { BlogUserRepository } from '@project/blog-user';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { BlogUserEntity, BlogUserRepository } from '@project/blog-user';
+import { CreateUserDto } from './dto/create-user.dto';
+import { AuthUser } from '../const';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Injectable()
 export class AuthenticationService {
   constructor(private readonly blogUserRepository: BlogUserRepository) {}
+
+  public async register(dto: CreateUserDto) {
+    const { email, firstName, lastName, password, avatarPath } = dto;
+
+    const blogUser = {
+      email,
+      firstName,
+      lastName,
+      avatarPath: avatarPath ?? '',
+      passwordHash: '',
+    };
+
+    const existUser = await this.blogUserRepository.findByEmail(email);
+
+    if (existUser) {
+      throw new ConflictException(AuthUser.Exists);
+    }
+
+    const userEntity = await new BlogUserEntity(blogUser).setPassword(password);
+
+    this.blogUserRepository.save(userEntity);
+
+    return userEntity;
+  }
+
+  public async verifyUser(dto: LoginUserDto) {
+    const { email, password } = dto;
+    const existUser = await this.blogUserRepository.findByEmail(email);
+
+    if (!existUser) {
+      throw new NotFoundException(AuthUser.NotFound);
+    }
+
+    if (!(await existUser.comparePassword(password))) {
+      throw new UnauthorizedException(AuthUser.PasswordWrong);
+    }
+
+    return existUser;
+  }
+
+  public async getUser(id: string) {
+    return this.blogUserRepository.findById(id);
+  }
 }
