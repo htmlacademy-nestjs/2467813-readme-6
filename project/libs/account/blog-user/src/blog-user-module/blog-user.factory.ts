dfc854771416cf73c5c1a849221cfe393d279753
Mikhail <84973540+Mikhail39yanov@users.noreply.github.com>
2024-04-02 13:40:37 +0200

    add lib data-access and BlogUserRepository

--- project/libs/account/blog-user/src/blog-user-module/blog-user.factory.ts
+++ project/libs/account/blog-user/src/blog-user-module/blog-user.factory.ts
@@ -1,6 +1,8 @@
 import { IAuthUser, IEntityFactory } from '@project/core';
 import { BlogUserEntity } from './blog-user.entity';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class BlogUserFactory implements IEntityFactory<BlogUserEntity> {
   public create(entityPlainData: IAuthUser): BlogUserEntity {
     return new BlogUserEntity(entityPlainData);
