d26a9b69aba3e5f7dd4875a68e6f3e435ff367bb
Mikhail <84973540+Mikhail39yanov@users.noreply.github.com>
2024-05-02 11:49:45 +0200

    update file

--- project/libs/file/file-uploader/src/const/const.ts
+++ project/libs/file/file-uploader/src/const/const.ts
@@ -1,5 +1,5 @@
 export const UploadResponseMessage = {
   UploadSuccess: 'The file has been uploaded successfully',
   FileNotFound: 'File not found',
-  FileFound: 'User found',
+  FileFound: 'File found',
 } as const;
