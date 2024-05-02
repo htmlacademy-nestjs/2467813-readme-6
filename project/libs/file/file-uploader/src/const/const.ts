032caab5920994cafa9aefd8ca6179f8b1972ec8
Mikhail <84973540+Mikhail39yanov@users.noreply.github.com>
2024-05-02 10:56:08 +0200

    add open api message file

---
+++ project/libs/file/file-uploader/src/const/const.ts
@@ -0,0 +1,5 @@
+export const UploadResponseMessage = {
+  UploadSuccess: 'The file has been uploaded successfully',
+  FileNotFound: 'File not found',
+  FileFound: 'User found',
+} as const;
--- project/libs/file/file-uploader/src/rdo/uploaded-file.rdo.ts
+++ project/libs/file/file-uploader/src/rdo/uploaded-file.rdo.ts
@@ -1,21 +1,47 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
+import { OpenApiMessages } from '../const';
 
 export class UploadedFileRdo {
+  @ApiProperty({
+    description: OpenApiMessages.id.description,
+    example: OpenApiMessages.id.example,
+  })
   @Expose()
   public id: string;
 
+  @ApiProperty({
+    description: OpenApiMessages.originalName.description,
+    example: OpenApiMessages.originalName.example,
+  })
   @Expose()
   public originalName: string;
 
+  @ApiProperty({
+    description: OpenApiMessages.hashName.description,
+    example: OpenApiMessages.hashName.example,
+  })
   @Expose()
   public hashName: string;
 
+  @ApiProperty({
+    description: OpenApiMessages.subDirectory.description,
+    example: OpenApiMessages.subDirectory.example,
+  })
   @Expose()
   public subDirectory: string;
 
+  @ApiProperty({
+    description: OpenApiMessages.mimetype.description,
+    example: OpenApiMessages.mimetype.example,
+  })
   @Expose()
   public mimetype: string;
 
+  @ApiProperty({
+    description: OpenApiMessages.size.description,
+    example: OpenApiMessages.size.example,
+  })
   @Expose()
   public size: number;
 }
