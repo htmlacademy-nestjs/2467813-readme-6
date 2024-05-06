b120b9b266344d1c7b108463a5fefda746fa814c
Mikhail <84973540+Mikhail39yanov@users.noreply.github.com>
2024-05-06 09:47:27 +0200

    add lib interceptors

---
+++ project/libs/shared/interceptors/src/lib/inject-user-id.interceptor.ts
@@ -0,0 +1,10 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+
+export class InjectUserIdInterceptor implements NestInterceptor {
+  public intercept(context: ExecutionContext, next: CallHandler) {
+    const request = context.switchToHttp().getRequest();
+    request.body['userId'] = request.user.sub;
+
+    return next.handle();
+  }
+}
